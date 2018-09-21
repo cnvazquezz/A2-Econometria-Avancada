fc4b8ab3f55ca76534dd3c3ea4b371df4ba4df90
Camilo <42455224+cnvazquezz@users.noreply.github.com>
2018-09-21 17:56:02 -0300

    Update A2-Econometria-Avançada.R

--- A2-Econometria-Avançada.R
+++ A2-Econometria-Avançada.R
@@ -1,35 +1,35 @@
-# Econometria Avançada A2
+# Econometria AvanÃ§ada A2
 
-# Noções Básicas - RSTUDIO
+# NoÃ§Ãµes BÃ¡sicas - RSTUDIO
 
 x <- 1:10 # utilizado para criar objeto x com elementos de 1:10
-y <- rep(3,10) # utilizado para criar objeto y com 10 repetiÃ§Ãµes do nÃºmero 3
-ls() #lista todos os objetos que estÃo no campo "global environment"
+y <- rep(3,10) # utilizado para criar objeto y com 10 repetiÃ§Ãµes do numero 3
+ls() #lista todos os objetos que estÃƒo no campo "global environment"
 rm(list="x","y") #apaga os objetos x e //y
 rm(list = ls()) #Apaga todos os dados em console
 setwd("G:/USJT/Econometria") #altera a pasta de trabalho do R para salvar
 getwd() #comando que mostra a pasta de trabalho atual do R
 
-# Para Instalar os Pacotes de utilização
+# Para Instalar os Pacotes de utilizaÃ§Ã£o
 
 install.packages("pwt8") #instala o pacote pwt8
 library("pwt8") # Mecanismo de START pacote pwt8
-data("pwt8.0") #carrega os dados pwt8.0 disponÃ­veis no pacote
+data("pwt8.0") #carrega os dados pwt8.0 disponiveis no pacote
 View (pwt8.0) #vizualiza a tabela pwt8.0
 
 # Comando para extrair os dados da tabela
 
-br <- subset(pwt8.0, country=="Brazil", select = c("rgdpna","avh","xr")) #cria a tabela br onde estão escolhido o pais Brasil e as variaveis
+br <- subset(pwt8.0, country=="Brazil", select = c("rgdpna","avh","xr")) #cria a tabela br onde estÃ£o escolhido o pais Brasil e as variaveis
 colnames(br) <-  c("PIB","Trabalho","Cambio") #altera o nome das variaveis
 
 # Comando para Contruit Graficos
 
-plot(br$PIB) #cria um grafico através dos valores selecionados de PIB na tabela br
+plot(br$PIB) #cria um grafico atravÃ©s dos valores selecionados de PIB na tabela br
 plot(br$PIB, type = "l") #Converte os valores em forma de linha
-dados <- ts(br, start = 1950, frequency = 1) #Monta a tabela dados como série temporal que se inicia em no ano de 1950, com frequênci anual
+dados <- ts(br, start = 1950, frequency = 1) #Monta a tabela dados como sÃ©rie temporal que se inicia em no ano de 1950, com frequÃªnci anual
 plot(dados) #Monta um grafico detabelasde dados
-plot(dados, col="blue", main="Dados Brasileiros", xlab="Ano") #muda o tÃ­tulo e a cor das linhas do grÃ¡fico para azul
+plot(dados, col="blue", main="Dados Brasileiros", xlab="Ano") #muda o titulo e a cor das linhas do grafico para azul
 
-#Salvar
+#Salvar CNVAZUEZ
 
 write.csv(br, file="C:/Econometria/Dados-Brasileiros.csv") #salva a tabela
